0915fc80c2018e04774a46945bd32dea1eaaebaf
github-action-benchmark <github@users.noreply.github.com>
2023-03-04 13:04:13 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for f43f566b9a1790f2746cfc18666675cdbb08fa5d

---
+++ dev/bench/data.js
@@ -0,0 +1,104 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1677935053734,
+  "repoUrl": "https://github.com/lechu445/LightXmlWriter",
+  "entries": {
+    "Benchmark.Net Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "lechu445@users.noreply.github.com",
+            "name": "Leszek B",
+            "username": "lechu445"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f43f566b9a1790f2746cfc18666675cdbb08fa5d",
+          "message": "Update benchmark.yml",
+          "timestamp": "2023-03-04T13:52:07+01:00",
+          "tree_id": "c081169c552bd2654c4caf2d2d0881cdbcc6b691",
+          "url": "https://github.com/lechu445/LightXmlWriter/commit/f43f566b9a1790f2746cfc18666675cdbb08fa5d"
+        },
+        "date": 1677935053255,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.LightXmlWriter_Write_Xml(MethodIndex: 0)",
+            "value": 560.204081632653,
+            "unit": "ns",
+            "range": "± 62.16007920752712"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.XmlWriter_Write_Xml(MethodIndex: 0)",
+            "value": 2722.680412371134,
+            "unit": "ns",
+            "range": "± 215.30267195236615"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.LightXmlWriter_Write_Xml(MethodIndex: 1)",
+            "value": 514.4329896907217,
+            "unit": "ns",
+            "range": "± 72.1539019527725"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.XmlWriter_Write_Xml(MethodIndex: 1)",
+            "value": 2261.5384615384614,
+            "unit": "ns",
+            "range": "± 139.65770119930198"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.LightXmlWriter_Write_Xml(MethodIndex: 2)",
+            "value": 687.3684210526316,
+            "unit": "ns",
+            "range": "± 71.81848464596074"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.XmlWriter_Write_Xml(MethodIndex: 2)",
+            "value": 2448.3406593406594,
+            "unit": "ns",
+            "range": "± 153.7440311239013"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.LightXmlWriter_Write_Xml(MethodIndex: 3)",
+            "value": 945.9081632653061,
+            "unit": "ns",
+            "range": "± 84.8503943256022"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.XmlWriter_Write_Xml(MethodIndex: 3)",
+            "value": 2282.3846153846152,
+            "unit": "ns",
+            "range": "± 155.34783689305246"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.LightXmlWriter_Write_Xml(MethodIndex: 4)",
+            "value": 1343,
+            "unit": "ns",
+            "range": "± 118.28317332109876"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.XmlWriter_Write_Xml(MethodIndex: 4)",
+            "value": 2422.548387096774,
+            "unit": "ns",
+            "range": "± 166.90009483214482"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.LightXmlWriter_Write_Xml(MethodIndex: 5)",
+            "value": 638.3737373737374,
+            "unit": "ns",
+            "range": "± 123.47928735329373"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.XmlWriter_Write_Xml(MethodIndex: 5)",
+            "value": 3167.3367346938776,
+            "unit": "ns",
+            "range": "± 237.59199819380015"
+          }
+        ]
+      }
+    ]
+  }
+}
