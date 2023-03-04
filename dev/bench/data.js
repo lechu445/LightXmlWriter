da827aca46460da4f7ed721e2139e3f690da03db
github-action-benchmark <github@users.noreply.github.com>
2023-03-04 14:17:43 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for f1fc0293397de5b4ba6aa4c32234bff83da1e454

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677935053734,
+  "lastUpdate": 1677939463440,
   "repoUrl": "https://github.com/lechu445/LightXmlWriter",
   "entries": {
     "Benchmark.Net Benchmark": [
@@ -96,6 +96,148 @@ window.BENCHMARK_DATA = {
             "value": 3167.3367346938776,
             "unit": "ns",
             "range": "± 237.59199819380015"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "xxx@example.com",
+            "name": "lechu445"
+          },
+          "committer": {
+            "email": "xxx@example.com",
+            "name": "lechu445"
+          },
+          "distinct": true,
+          "id": "f1fc0293397de5b4ba6aa4c32234bff83da1e454",
+          "message": "run tests on net7",
+          "timestamp": "2023-03-04T15:13:02+01:00",
+          "tree_id": "2bd279a305b10736e5ed1eba16e226487f811d20",
+          "url": "https://github.com/lechu445/LightXmlWriter/commit/f1fc0293397de5b4ba6aa4c32234bff83da1e454"
+        },
+        "date": 1677939462365,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.LightXmlWriter_Write_Xml(MethodIndex: 0)",
+            "value": 1048.060606060606,
+            "unit": "ns",
+            "range": "± 118.65898816818853"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.XmlWriter_Write_Xml(MethodIndex: 0)",
+            "value": 3947.294117647059,
+            "unit": "ns",
+            "range": "± 87.31391978507949"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.LightXmlWriter_Write_Xml(MethodIndex: 1)",
+            "value": 1283.1,
+            "unit": "ns",
+            "range": "± 111.94555855775643"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.XmlWriter_Write_Xml(MethodIndex: 1)",
+            "value": 4374.277777777777,
+            "unit": "ns",
+            "range": "± 200.15479072850673"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.LightXmlWriter_Write_Xml(MethodIndex: 2)",
+            "value": 2064.7083333333335,
+            "unit": "ns",
+            "range": "± 165.38452270656163"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.XmlWriter_Write_Xml(MethodIndex: 2)",
+            "value": 4682.333333333333,
+            "unit": "ns",
+            "range": "± 177.50008648379406"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.LightXmlWriter_Write_Xml(MethodIndex: 3)",
+            "value": 2110.04,
+            "unit": "ns",
+            "range": "± 172.33757804982864"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.XmlWriter_Write_Xml(MethodIndex: 3)",
+            "value": 5100.25,
+            "unit": "ns",
+            "range": "± 243.61024894250755"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.LightXmlWriter_Write_Xml(MethodIndex: 4)",
+            "value": 2779.030303030303,
+            "unit": "ns",
+            "range": "± 149.3881850643813"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.XmlWriter_Write_Xml(MethodIndex: 4)",
+            "value": 5367.882352941177,
+            "unit": "ns",
+            "range": "± 185.30364347525654"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.LightXmlWriter_Write_Xml(MethodIndex: 5)",
+            "value": 1612.801886792453,
+            "unit": "ns",
+            "range": "± 83.3005644328602"
+          },
+          {
+            "name": "XmlTools.Benchmarks.SimpleBenchmarks.XmlWriter_Write_Xml(MethodIndex: 5)",
+            "value": 4808.0921052631575,
+            "unit": "ns",
+            "range": "± 268.21976947429147"
+          },
+          {
+            "name": "XmlTools.Benchmarks.Simple_XML.LightXmlWriter_Write_Xml",
+            "value": 6804.192307692308,
+            "unit": "ns",
+            "range": "± 184.255478991702"
+          },
+          {
+            "name": "XmlTools.Benchmarks.Simple_XML.XmlWriter_Write_Xml",
+            "value": 19040.975,
+            "unit": "ns",
+            "range": "± 674.793054221545"
+          },
+          {
+            "name": "XmlTools.Benchmarks.Creating_Writer.LightXmlWriter_StreamWriter",
+            "value": 15.701940923929214,
+            "unit": "ns",
+            "range": "± 0.503715095989869"
+          },
+          {
+            "name": "XmlTools.Benchmarks.Creating_Writer.XmlWriter_StreamWriter",
+            "value": 1248.4864615607507,
+            "unit": "ns",
+            "range": "± 93.08083047614635"
+          },
+          {
+            "name": "XmlTools.Benchmarks.Creating_Writer.XmlWriter_Stream",
+            "value": 848.4750992107391,
+            "unit": "ns",
+            "range": "± 103.15412384136518"
+          },
+          {
+            "name": "XmlTools.Tests.Benchmarks.OTA_Standard_XML.LightXmlWriter_Write_Xml",
+            "value": 16238.827586206897,
+            "unit": "ns",
+            "range": "± 898.737605689956"
+          },
+          {
+            "name": "XmlTools.Tests.Benchmarks.OTA_Standard_XML.XmlWriter_Write_Xml",
+            "value": 40116.28571428572,
+            "unit": "ns",
+            "range": "± 414.8348009604692"
+          },
+          {
+            "name": "XmlTools.Tests.Benchmarks.OTA_Standard_XML.XmlWriter_From_Stream_Write_Xml",
+            "value": 39543.583333333336,
+            "unit": "ns",
+            "range": "± 614.1846714189965"
           }
         ]
       }
